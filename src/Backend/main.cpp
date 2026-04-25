99da75a8147c91c60cb76adfcaa25a44547c446c
Oscar <3055716560301@ingenieria.usac.edu.gt>
2026-04-25 06:33:59 -0600

    feat
    cambios

--- src/Backend/main.cpp
+++ src/Backend/main.cpp
@@ -204,6 +204,6 @@ int main() {
             CorsPreflightHandler(req, res);
         });
 
-    app.port(8080).multithreaded().run();
+    app.bindaddr("0.0.0.0").port(8080).multithreaded().run();
     return 0;
 }
